55b9dd7139de2dfb16e293aa0fd435f0260d8d0e
Marcus Rowe <undisbeliever@gmail.com>
2010-12-18 22:58:18 +1000

    Created the initial file hasher.

--- recent_additions_updator.py
+++ recent_additions_updator.py
@@ -29,4 +29,53 @@ where parameters can be:
 
 """
 
+import hashlib
+import os
+
+def partial_shasum(filename, clip):
+    """ Preforms a partial shasum for a given filename, clipping the file at
+        clip bytes.
+    """
+    # ::TESTED 20101218 21:42 ::
+    s = hashlib.sha1()
+    fp = open(filename, 'rb')
+
+    while True:
+        # end of clipping boundary
+        if fp.tell() < clip - 4096:
+            b = fp.read(clip - fp.tell())
+            s.update(b)
+            break
+        b = fp.read(4096)
+        s.update(b)
+        # EOF
+        if len(b) < 4096:
+            break; 
+
+    fp.close()
+    return s
+
+
+def locate(root):
+    """ Generator that recursivly lists all of the files in a given directory. """
+    # ::TESTED 20101218 2208 ::
+    for path, dirs, basenames in os.walk(root):
+        for basename in basenames:
+            yield os.path.join(path, basename)
+
+
+def create_sizehashes(source_dirs):
+    """ Returns a dict for each file in the source directories.
+
+        The dict will have the filename as the key, and a tuple
+        (size, partial shasum)
+    """
+    sizehashes = dict()
+    for d in source_dirs:
+        for f in locate(d):
+            hash = partial_shasum(f, 1024*1024*10)
+            size = os.path.getsize(f)
+            
+            value = (size, hash.digest())
+            sizehashes[f] = value
 
