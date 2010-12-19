976cf6782ff23f18b815ebfc09b0930a1ff7bc84
Marcus Rowe <undisbeliever@gmail.com>
2010-12-19 18:41:49 +1000

    Started on the creation of the recent additions directory
    need to work on:
     - link name collisions
     - last ..., this ... links
     - 0th week, last week links?

--- recent_additions_updator.py
+++ recent_additions_updator.py
@@ -16,20 +16,19 @@ files size.
 The format of the datafile is as thus;
 
 <parameter> = <value>
-<file> <size> <partial shasum>
+<partial shasum> <size> <file>
 
 where parameters can be:
- * scan - the directory to scan (this paramter can be used multiple times).
- * target - the location of the 'recent additions' directory
+ * scan    - the directory to scan (this paramter can be used multiple times).
+ * target  - the location of the 'recent additions' directory
  * shaclip - the number of megabytes (in the beginning of the file)
-             the shasum is created from. 
- * days - the number of days before a file is not recent anymore.
- * links - use soft or hard links (default is soft, but hard links may be
-           required for samba/nfs shares.
-
+                the shasum is created from.
+ * minsize - the minimum size of a file that end up in the recent additions
+                directory.
 """
 
 import hashlib
+import time
 import os
 
 def partial_shasum(filename, clip):
@@ -64,18 +63,69 @@ def locate(root):
             yield os.path.join(path, basename)
 
 
-def create_sizehashes(source_dirs):
-    """ Returns a dict for each file in the source directories.
 
-        The dict will have the filename as the key, and a tuple
-        (size, partial shasum)
+def create_links(filename, target, format, t=None):
+    if t is None:
+        t = time.localtime()
+        print "HI"
+    dir = time.strftime(format, t)
+    dir = os.path.join(target, dir)
+    # create directory (if necessary)    
+    if os.path.isdir(dir) is False:
+        os.mkdir(dir)
+    link = os.path.basename(filename)
+    link = os.path.join(dir, link)
+    # make link
+    if os.path.exists(link):
+        print "MATCH: %s %s" % (filename, link)
+    else:
+        os.symlink(filename, link)
+
+
+def create_initial_directory(sources, datafile, target,
+        minsize=1024*256, shaclip=1024*1024*10):
+    """ Scans the source directories and creates the datafile, while populating
+        the target 'recent additions' directory.
+
+        sources, target, minsize and shaclip are the settings of the 'recent
+        additions' directory, and will be saved into the datafile.
+
+        Raises OSError if the target or datafile cannot be created, or if the
+            sources cannot be scanned.
+        Raises Error if the target directory or the datafile already exists.
     """
-    sizehashes = dict()
-    for d in source_dirs:
-        for f in locate(d):
-            hash = partial_shasum(f, 1024*1024*10)
-            size = os.path.getsize(f)
-            
-            value = (size, hash.digest())
-            sizehashes[f] = value
+    if os.path.exists(target):
+        raise Exception("Directory %s already exists." % target)
+    if os.path.exists(datafile):
+        raise Exception("Datafile %s already exists." % target)
 
+    fp = open(datafile, 'w')
+    # copy settings to datafile
+    for d in sources:
+        fp.write("scan = %s\n" % os.path.abspath(d))
+    fp.write("target = %s\n" %target)
+    fp.write("shaclip = %d\n" % shaclip)
+    fp.write("minsize = %d\n" % minsize)
+
+    # create recent updates directory 
+    os.mkdir(target)
+    os.mkdir(os.path.join(target, 'month'))
+    os.mkdir(os.path.join(target, 'week'))
+    os.mkdir(os.path.join(target, 'day'))
+    # scan sources
+    for d in sources:
+        for f in locate(d):
+            f = os.path.abspath(f)
+            hash = partial_shasum(f, shaclip)
+            size = os.path.getsize(f)
+            mtime = time.localtime(os.path.getmtime(f))
+
+            if size > minsize:
+                create_links(f, target, 'month/%Y-%m', mtime)
+                create_links(f, target, 'week/%Y-%U', mtime)
+                create_links(f, target, 'day/%Y-%m-%d', mtime)
+
+            fp.write("%s %d %s\n" % (hash.hexdigest(), size, f))
+    fp.close()
+
+create_initial_directory(['Video'], 'datafile.txt', 'recent')
