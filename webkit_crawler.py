f55ccfc0529575e6718e0b75d3f75f5e68f29401
Marcus Rowe <undisbeliever@gmail.com>
2011-07-19 01:44:12 +1000

    Added cookie support. Imporved page checking.

--- webkit_crawler.py
+++ webkit_crawler.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# vim: set fenc=utf-8 ai ts=4 sw=4 sts=4 et:
+
 
 import os
 import sys
 import signal
-import urllib2
+from urlparse import urlparse
+from httplib import HTTPConnection
 
 from PyQt4.QtCore import QUrl, SIGNAL
 from PyQt4.QtGui import QApplication
-from PyQt4.QtNetwork import QNetworkProxy
+from PyQt4.QtNetwork import QNetworkProxy, QNetworkCookie, QNetworkCookieJar
 from PyQt4.QtWebKit import QWebPage
 
 
+#::SOURCE http://stackoverflow.com/questions/5423013/pyqt-how-to-use-qwebpage-with-an-anonimous-proxy/5564898#5564898::
 def set_proxy(proxy):
     proxy_url = QUrl(proxy)
     if unicode(proxy_url.scheme()).startswith('http'):
@@ -28,14 +33,32 @@ if 'HTTP_PROXY' in os.environ:
     set_proxy(os.environ['HTTP_PROXY'])
 
 
+#::KUDOS Tudor Barbu <ul.enatom@uaim> ::
+#::: http://blog.motane.lu/2009/07/07/downloading-a-pages-content-with-python-and-webkit/ ::
 class Crawler( QWebPage ):
     def __init__(self):
         QWebPage.__init__(self)
 
     def crawl(self, url):
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	self.connect(self, SIGNAL('loadFinished(bool)'), self._finished_loading)
-	self.mainFrame().load(QUrl(url))
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        self.connect(self, SIGNAL('loadFinished(bool)'), self._finished_loading)
+        self.mainFrame().load(QUrl(url))
+
+    def load_cookies(self, url, cookie_strings):
+        p = urlparse(url)
+
+        cookies = list()
+        for c in cookie_strings:
+            n = c.find('=')
+            name, value = c[:n], c[n+1:]
+            cookie = QNetworkCookie(name, value)
+            cookie.setDomain(p.netloc)
+            cookies.append(cookie)
+
+        cookiejar = QNetworkCookieJar()
+        cookiejar.setAllCookies(cookies)
+        self.networkAccessManager().setCookieJar(cookiejar)
+
 
     def _finished_loading( self, result ):
         """ When done loading, pring the result. """
@@ -43,33 +66,39 @@ class Crawler( QWebPage ):
 	sys.exit(0)
 
 
+def test_url(url):
+    """ Tests if given url returns a valid response."""
+    p = urlparse(url)
+    conn = HTTPConnection(p.netloc, p.port)
+    conn.request('HEAD', p.path)
+    res = conn.getresponse()
+
+    if res.status > 300 and res.status < 399:
+        test_url(res.getheader('location'))
+        return
+    if res.status < 200 or res.status > 399:
+        sys.stderr.write("%d: %s\n" % (res.status, res.reason))
+        sys.exit(1)
+
+
 def main():
     app = QApplication([])
 
-    if len(sys.argv) != 2:
-        sys.stderr.write("Expected argument url\n")
+    if len(sys.argv) < 2:
+        sys.stderr.write("Arguments: url [cookie_name=value ...]\n")
         sys.exit()
 
     url = sys.argv[1]
+
+    test_url(url)
+
     crawler = Crawler()
-
-    qurl = QUrl(url)
-    user_agent = crawler.userAgentForUrl(qurl)
-
-    uo = urllib2.build_opener()
-    uo.addheaders = [('User-agent', user_agent)]
-
-    try:
-        up = uo.open(url)
-        up.close()
-    except urllib2.HTTPError as e:
-        sys.stderr.write("HTTPError" + str(e.code) + "\n")
-        del crawler # prevent segfault
-        sys.exit(1)
-
+    if len(sys.argv) > 2:
+        crawler.load_cookies(url, sys.argv[2:])
     crawler.crawl(url)
 
     sys.exit( app.exec_() )
+
 
 if __name__ == '__main__':
 	main()
